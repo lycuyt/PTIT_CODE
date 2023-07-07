022e4c910e4e137c0360a75cec67a3e5e965f2d2
lycuyt <109753738+lycuyt@users.noreply.github.com>
2023-07-07 21:33:55 +0700

    Update dichuyentrongmecung2.cpp

--- DSA/QUAY LUI- NHÁNH CẬN/dichuyentrongmecung2.cpp
+++ DSA/QUAY LUI- NHÁNH CẬN/dichuyentrongmecung2.cpp
@@ -1,67 +1,57 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int a[100][100], n;
+int n;
+int a[100][100];
 bool vs[100][100];
 vector<string> res;
-
-void Try(int i, int j,string s){
-	if(a[1][1]==0 || a[n][n]==0){
-		
+void Try(int i, int j, string s){
+	if(a[1][1]== 0 || a[n][n]==0){
 		return;
 	}
-	if(i==n && j==n ){
+	if(i==n && j==n){
 		res.push_back(s);
 	}
-	if(i!=n && a[i+1][j] && !vs[i+1][j]){
-		vs[i+1][j]=true;
+	if( a[i+1][j]&& i!=n && !vs[i+1][j]){
+		vs[i][j]=true;
 		Try(i+1,j,s+"D");
-		vs[i+1][j]=false;
+		vs[i][j]=false;
 	}
-	if(j!=n && a[i][j+1] && !vs[i][j+1]){
-		vs[i][j+1]=true;
+	if( a[i][j+1] && j!=n && !vs[i][j+1]){
+		vs[i][j]=true;
 		Try(i,j+1,s+"R");
-		vs[i][j+1]=false;
+		vs[i][j]=false;
 	}
-	if(j != 1 && a[i][j-1] && !vs[i][j-1]){
-		vs[i][j-1]=true;
+	if( a[i][j-1] && j != 1  && !vs[i][j-1]){
+		vs[i][j]=true;
 		Try(i,j-1,s+"L");
-		vs[i][j-1]=false;
+		vs[i][j]=false;
 	}
 	
 	
-	if(i!=1  && a[i-1][j]&& !vs[i-1][j]){
-		vs[i-1][j]=true;
+	if( a[i-1][j] && i!=1  && !vs[i-1][j]){
+		vs[i][j]=true;
 		Try(i-1,j,s+"U");
-		vs[i-1][j]=false;
+		vs[i][j]=false;
 	}
-	if((i<n && j<n && !a[i+1][j] && !a[i][j+1] && !a[i-1][j] && !a[i][j-1] )||i>n ||j>n){
-		return;
-	}
+	
 }
-
-
 int main()
-{
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		//nhap mang
-		res.clear();
+ {
+ 	int t;cin>>t;
+ 	while(t--)
+ 	{
+ 		//phan tich so 1
+		
 		cin>>n;
+		
 		for(int i=1;i<=n;i++){
 			for(int j=1;j<=n;j++){
 				cin>>a[i][j];
-				vs[i][j]==false;
 			}
 		}
-		
-		
-		
-		// dequy
+		memset(vs,false,sizeof(vs));
+		res.clear();
 		Try(1,1,"");
-		//kiemtra va in
 		if(res.size()==0){
 			cout<<-1;
 		}else{
@@ -72,6 +62,6 @@ int main()
 		}
 		cout<<endl;
 		
-		
 	}
-}
+
+ }
